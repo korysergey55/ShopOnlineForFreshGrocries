d6c78b600f7601f2fc2a69b308ec035d33aa1d20
Serhiy Korytskyy <korysergey55@gmail.com>
2021-09-14 18:10:17 +0300

    Updates

---
+++ precache-manifest.a7990c64e83e7f5cbf0dc501fed84648.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463c1536e2a30c6be26c48a3919de05d",
+    "url": "/ShopOnlineForFreshGrocries/index.html"
+  },
+  {
+    "revision": "8be4b4969f0c6f33c9a4",
+    "url": "/ShopOnlineForFreshGrocries/static/css/2.5cc265e8.chunk.css"
+  },
+  {
+    "revision": "0751bbcb03aedacba353",
+    "url": "/ShopOnlineForFreshGrocries/static/css/main.25e15d70.chunk.css"
+  },
+  {
+    "revision": "8be4b4969f0c6f33c9a4",
+    "url": "/ShopOnlineForFreshGrocries/static/js/2.14d35fba.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/ShopOnlineForFreshGrocries/static/js/2.14d35fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0751bbcb03aedacba353",
+    "url": "/ShopOnlineForFreshGrocries/static/js/main.982dc4d7.chunk.js"
+  },
+  {
+    "revision": "fc8b0953485731db77dc",
+    "url": "/ShopOnlineForFreshGrocries/static/js/runtime-main.3d792e6a.js"
+  },
+  {
+    "revision": "e0ed080b32bd91ccba7a68e9c248e086",
+    "url": "/ShopOnlineForFreshGrocries/static/media/Layer.e0ed080b.png"
+  },
+  {
+    "revision": "4546b2714e1e1a7d99a8ce7c9cfd046b",
+    "url": "/ShopOnlineForFreshGrocries/static/media/supermarket-paper-bag-full-healthy-food.4546b271.jpg"
+  }
+]);
