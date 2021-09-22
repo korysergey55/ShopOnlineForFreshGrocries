c18a397240c7508b9396a267188d1684a2e6a83c
Serhiy Korytskyy <korysergey55@gmail.com>
2021-09-22 10:16:48 +0300

    header-hero-done

---
+++ src/containers/Public/ProductDetailsPage/HeroProductsDatails/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+const HeroProductsDatails = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.heroContainer}>
+          <h3 className={styles.subtitle}>Home Product Detail</h3>
+          <h2 className={styles.title}>Product Detail</h2>
+        </div>
+        <div className={styles.img}></div>
+      </div>
+    </>
+  )
+}
+
+export default HeroProductsDatails
--- src/containers/Public/ProductDetailsPage/index.tsx
+++ src/containers/Public/ProductDetailsPage/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import styles from './styles.module.scss'
 import Header from 'components/Header'
+import HeroProductsDatails from '../ProductDetailsPage/HeroProductsDatails/index'
 const index = () => {
   return (
     <>
       <div className={styles.headerHeroContainer}>
         <div className={styles.headerLeftBg}></div>
         <Header />
-        {/* <Hero /> */}
+        <HeroProductsDatails />
       </div>
       <h2 className="title">ProductDetailsPage</h2>
     </>
