ce227fa53c02491f6175ec6314c0e9ca44bb1cc7
Serhiy Korytskyy <korysergey55@gmail.com>
2021-09-24 14:21:12 +0300

    refactoring

--- src/containers/Public/ProductDetailsPage/PhotoList/index.tsx
+++ src/containers/Public/ProductDetailsPage/PhotoList/index.tsx
@@ -4,29 +4,20 @@ import styles from './styles.module.scss'
 
 interface IProps {
   item?: string
- 
- }
-// const contentStyle: any = {
-//   height: '160px',
-//   color: '#fff',
-//   lineHeight: '160px',
-//   textAlign: 'center',
-//   background: '#364d79',
-// }
+}
+
 const PhotoList: React.FC<IProps> = ({ item }) => {
   const { productStore } = useStore()
   const changeFoto = () => {
-    console.log('changed')
     productStore.setFoto(item)
-    console.log(item)
   }
   return (
     <>
-      <li  className={styles.li} onClick={changeFoto}>
+      <li className={styles.li} onClick={changeFoto}>
         <img className={styles.img} src={item} alt=""></img>
       </li>
     </>
   )
 }
 
-export default PhotoList;
+export default PhotoList
--- src/stores/Products.ts
+++ src/stores/Products.ts
@@ -11,9 +11,9 @@ import {
 import IProduct from '../models/product'
 
 class Products {
-  @observable trendingProducts: any = []
-  @observable bestProducts: any = []
-  @observable relatedProducts: any = []
+  @observable trendingProducts: IProduct[] = []
+  @observable bestProducts: IProduct[] = []
+  @observable relatedProducts: IProduct[] = []
   @observable foto: string | undefined = ''
 
   constructor() {
